b27d83253c840a9e5480e430794ca89ad583f3d2
dmparnak <dmparnak@gmail.com>
2023-04-28 01:18:35 +0300

    Upd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Outlet,
-  Navigate,
-  Routes
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+    Outlet,
+    Navigate,
+    Routes
 } from "react-router-dom";
 import Login from "./login/Login";
 import Category from './Category';
@@ -14,97 +14,106 @@ import Checks from './Checks';
 import Customers from './Customers';
 import Menu from './Menu';
 import ProductsInTheShop from './ProductsInTheShop';
-import { useState } from "react";
+import {useState} from "react";
 
 const userRoles = {
-  manager: {
-    category: true,
-    employee: true,
-    products: true,
-    checks: false,
-    customers: true,
-    productsInShop: true,
-  },
-  cashier: {
-    category: false,
-    employee: false,
-    products: false,
-    checks: true,
-    customers: false,
-    productsInShop: true,
-  },
+    manager: {
+        category: true,
+        employee: true,
+        products: true,
+        checks: false,
+        customers: true,
+        productsInShop: true,
+    },
+    cashier: {
+        category: false,
+        employee: false,
+        products: false,
+        checks: true,
+        customers: false,
+        productsInShop: true,
+    },
 };
 
 function App() {
-  const [user, setUser] = useState(null);
+    const [user, setUser] = useState(localStorage.getItem("userRole") || null);
 
-  const hasAccess = (path) => {
-    if (!user) return false;
-    const resource = path.replace(/^\//, '');
-    return userRoles[user][resource] || false;
-  };
+    const handleLogout = () => {
+        setUser(null);
+        localStorage.removeItem("userRole");
+    };
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: user ? <Menu /> : <Login onLogin={(role) => setUser(role)} />,
-      children: [
+
+    const hasAccess = (path) => {
+        if (!user) return false;
+        const resource = path.replace(/^\//, '');
+        return userRoles[user][resource] || false;
+    };
+
+    const router = createBrowserRouter([
         {
-          path: "*",
-          element: (
-              <Routes>
-                <ProtectedRoute
-                    path="category"
-                    hasAccess={hasAccess}
-                    element={<Category />}
-                />
-                <ProtectedRoute
-                    path="employee"
-                    hasAccess={hasAccess}
-                    element={<Employee />}
-                />
-                <ProtectedRoute
-                    path="products"
-                    hasAccess={hasAccess}
-                    element={<Products />}
-                />
-                <ProtectedRoute
-                    path="checks"
-                    hasAccess={hasAccess}
-                    element={<Checks />}
-                />
-                <ProtectedRoute
-                    path="customers"
-                    hasAccess={hasAccess}
-                    element={<Customers />}
-                />
-                <ProtectedRoute
-                    path="productsInShop"
-                    hasAccess={hasAccess}
-                    element={<ProductsInTheShop />}
-                />
-              </Routes>
-          ),
+            path: "/",
+            element: user ? <Menu onLogout={handleLogout}/> : <Login onLogin={(role) => {
+                setUser(role);
+                localStorage.setItem("userRole", role);
+            }}/>,
+            children: [
+                {
+                    path: "*",
+                    element: (
+                        <Routes>
+                            <ProtectedRoute
+                                path="category"
+                                hasAccess={hasAccess}
+                                element={<Category/>}
+                            />
+                            <ProtectedRoute
+                                path="employee"
+                                hasAccess={hasAccess}
+                                element={<Employee/>}
+                            />
+                            <ProtectedRoute
+                                path="products"
+                                hasAccess={hasAccess}
+                                element={<Products/>}
+                            />
+                            <ProtectedRoute
+                                path="checks"
+                                hasAccess={hasAccess}
+                                element={<Checks/>}
+                            />
+                            <ProtectedRoute
+                                path="customers"
+                                hasAccess={hasAccess}
+                                element={<Customers/>}
+                            />
+                            <ProtectedRoute
+                                path="productsInShop"
+                                hasAccess={hasAccess}
+                                element={<ProductsInTheShop/>}
+                            />
+                        </Routes>
+                    ),
+                },
+            ],
         },
-      ],
-    },
-  ]);
+    ]);
 
-  return (
-      <div className="App">
-        <RouterProvider router={router}>
-          <Outlet />
-        </RouterProvider>
-      </div>
-  );
+    return (
+        <div className="App">
+            <RouterProvider router={router}>
+                <Outlet/>
+            </RouterProvider>
+        </div>
+    );
 }
 
-const ProtectedRoute = ({ path, hasAccess, element }) => {
-  if (hasAccess(path)) {
-    return <Route path={path} element={element} />;
-  } else {
-    return <Navigate to="/" />;
-  }
+const ProtectedRoute = ({path, hasAccess, element}) => {
+    if (hasAccess(path)) {
+        return <Route path={path} element={element}/>;
+    } else {
+        return <Navigate to="/"/>;
+    }
 };
 
 export default App;
